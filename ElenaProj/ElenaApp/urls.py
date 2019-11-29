9687418c58b6cd8adbe4d6cda901178658ce9ec2
Eden Mendel <mendeleden@gmail.con>
2019-11-29 13:19:11 -0500

    clean dir

--- ElenaProj/ElenaApp/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import LocationViewSet
-
-router = routers.DefaultRouter()
-router.register('api/location', LocationViewSet, 'location')
-
-urlpatterns = router.urls
