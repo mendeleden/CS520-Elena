42d435f6938bc7d48b6bcdde1f3ba556a2b2bfe4
Eden Mendel <mendeleden@gmail.con>
2019-12-06 12:21:08 -0500

    enableing cors

--- pip_setup.sh
+++ pip_setup.sh
@@ -2,7 +2,9 @@
 
 echo "Installing Django"
 pip install django
+echo "Installing Cors"
 pip install djangorestframework django-cors-headers
+pip install django-cors-middleware
 echo "Starting Server"
 cd osmbackend
 python manage.py runserver 0.0.0.0:8000
