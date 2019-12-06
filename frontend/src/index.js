478a0aa496ae8c75e71f1ab158c2c6c6e1f4765e
Zannzabar <danny.sanatar@gmail.com>
2019-12-06 12:43:46 -0500

    added get req to pull and update map info

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';       // lowkey same thing as original
 import './index.css';
 import App from './App';
@@ -20,6 +21,7 @@ import Map from './components/Map'
 import Sidebar from './components/Sidebar'
 import * as serviceWorker from './serviceWorker';
 import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHNhbmF0YXIiLCJhIjoiY2szcm81eTlpMGVndjNja2l5cGtobnRuMyJ9.0c8156xZy5h97w6Uq-f8wg';
 
@@ -42,11 +44,28 @@ class Application extends React.Component{
         });
     }
 
+    setRoute(res){
+        this.setState({ lng: res.data.from_lat[1], lat: res.data.from_lat[0], zoom: 12});
+        // zoom is hard coded rn
+        // we might want to write a function that calculates how zoomed it should be
+        // based on the two points
+
+        this.componentDidMount();
+    }
+
+    getRoute(){
+        axios
+        .get("http://ec2-3-85-127-123.compute-1.amazonaws.com:8000/simple/route/216,%20Pond%20Street,%20Natick,%20Massachusetts,%2001760,%20United%20States/14,%20Mill%20Street,%20Natick,%20Massachusetts,%2001760,%20United%20States")
+        .then(res => this.setRoute(res))
+        .catch(err => console.log(err));
+    }
+
+
     render(){
         return(
             <div>
                 <div ref={el => this.mapContainer = el} className="mapContainer" />
-
+                <Button onClick={() => this.getRoute()}>Test</Button>
                 <Map/>
                 <Sidebar/>
             </div>
