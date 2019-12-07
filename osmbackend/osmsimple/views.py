a81920bb1a8199aa599a823ac43fe7ddf2fc917f
Eden Mendel <mendeleden@gmail.con>
2019-12-07 13:48:31 -0500

    functions

--- osmbackend/osmsimple/views.py
+++ osmbackend/osmsimple/views.py
@@ -8,13 +8,46 @@ import osmnx as ox
 
 def get_geocodes(request, address_from=None, address_to=None):
     print(request)
-    from_lat = ox.geocode(address_from)
-    to_lat = ox.geocode(address_to)
+    from_lat = get_lon_lat(address_from)
+    to_lat = get_lon_lat(address_to)
     
+    if from_lat is None or to_lat is None:
+        return JsonResponse({'error' : False})
+        
     data = {
         'from': address_from,
         'to' : address_to,
         'from_lat' : from_lat,
         'to_lat' : to_lat,
+        'error' : False,
     }
     return JsonResponse(data)
+
+
+def get_lon_lat(address=None):
+    if address is None:
+        return None
+    else:
+        return ox.geocode(address)
+
+def get_graph(place=None):
+    if place is None:
+        return None
+    else:
+        G = ox.graph_from_place(['Amherst, Massachusetts, USA'])
+        return G
+
+
+'''
+start = ox.get_nearest_node(G, origin)
+end = ox.get_nearest_node(G, destination)
+route_by_length = nx.shortest_path(G, source=orig, target=dest, weight='length')
+nodes, data = zip(*G.nodes(data=True))
+dict_by_id = {}
+for x in data:
+    dict_by_id[x['osmid']] = x
+list_steps = []
+for step in route_by_length:
+    list_steps.append([dict_by_id[step]['x'], dict_by_id[step]['y']])
+print(list_steps)
+'''
