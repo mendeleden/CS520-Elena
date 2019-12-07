5988c5617851e2bebef813f1fbdfae98e93824d5
Eden Mendel <mendeleden@gmail.con>
2019-12-07 16:46:33 -0500

    adding data files

--- osmbackend/osmsimple/views.py
+++ osmbackend/osmsimple/views.py
@@ -5,49 +5,74 @@ from django.db.models import Q
 # Create your views here.
 
 import osmnx as ox
+import networkx as nx
+
+def get_graph():
+    key ="AIzaSyCLr4LXvIZlqL7D1x39-AlnvwOxz6ZafO8"
+    print("hey")
+    place_query = ['Natick, Massachusetts, USA']
+    G = ox.graph_from_place(place_query, network_type='walk')
+    G = ox.add_node_elevations(G, key)
+    G = ox.add_edge_grades(G)
+    # G_projected = ox.project_graph(G)
+    ox.save_graphml(G, filename='natick.graphml')
+    print("done")
+
+def load_graph():
+    G = ox.load_graphml('natick.graphml')
+    return G
+
+def geo_address(address):
+    return ox.geocode(address)
+
+def get_route(G, start, end):
+    route_by_length = nx.shortest_path(G, source=start, target=end, weight='length')
+    return route_by_length
+
+def convert_g_dic(G):
+    nodes, data = zip(*G.nodes(data=True))
+    dict_by_id = {}
+    
+    for x in data:
+        dict_by_id[x['osmid']] = x
+
+    return dict_by_id
+
+def get_steps(route, dic_by_id):
+    list_steps = []
+
+    for step in route:
+        list_steps.append([dic_by_id[step]['x'], dic_by_id[step]['y']])
+    return list_steps
 
 def get_geocodes(request, address_from=None, address_to=None):
     print(request)
-    from_lat = get_lon_lat(address_from)
-    to_lat = get_lon_lat(address_to)
     
-    if from_lat is None or to_lat is None:
-        return JsonResponse({'error' : False})
-        
+    origin = geo_address("216, Pond Street, Natick, Massachusetts, 01760, USA")
+    destination = geo_address("14, Mill Street, Natick, Massachusetts, 01760, USA")
+    
+    print("loading graph")
+    G = load_graph()
+    
+    start = ox.get_nearest_node(G, origin)
+    end = ox.get_nearest_node(G, destination)
+
+    route = get_route(G, start, end)
+    print("len of route: ", len(route))
+
+    dic_nodeid = convert_g_dic(G)
+
+    lat_lon_steps = get_steps(route, dic_nodeid)
+    print("length of steps: ", len(lat_lon_steps))
+
     data = {
         'from': address_from,
         'to' : address_to,
-        'from_lat' : from_lat,
-        'to_lat' : to_lat,
+        'from_lat' : origin,
+        'to_lat' : destination,
+        'node_ids' : (str(start), str(end)),
+        'route_nodeis' : str(route),
+        'lat_lon_steps' : str(lat_lon_steps),
         'error' : False,
     }
     return JsonResponse(data)
-
-
-def get_lon_lat(address=None):
-    if address is None:
-        return None
-    else:
-        return ox.geocode(address)
-
-def get_graph(place=None):
-    if place is None:
-        return None
-    else:
-        G = ox.graph_from_place(['Amherst, Massachusetts, USA'])
-        return G
-
-
-'''
-start = ox.get_nearest_node(G, origin)
-end = ox.get_nearest_node(G, destination)
-route_by_length = nx.shortest_path(G, source=orig, target=dest, weight='length')
-nodes, data = zip(*G.nodes(data=True))
-dict_by_id = {}
-for x in data:
-    dict_by_id[x['osmid']] = x
-list_steps = []
-for step in route_by_length:
-    list_steps.append([dict_by_id[step]['x'], dict_by_id[step]['y']])
-print(list_steps)
-'''
