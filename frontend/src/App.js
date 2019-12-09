005d6add44dae96013814879e9301535b6baa34c
Zannzabar <danny.sanatar@gmail.com>
2019-12-08 21:00:18 -0500

    i guess this branch is broken

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyMap from "./components/Map"
+import Map from "./components/Map"
 import LocationSearch from './components/LocationSearch';
 import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <LocationSearch />
-      <MyMap />
+      <Map />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,7 +25,6 @@ import axios from 'axios';
 import LocationSearch from './components/LocationSearch';
 
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
