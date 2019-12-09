620e0b96168e9493f442cb8e0013ea2b75aaf01b
Zannzabar <danny.sanatar@gmail.com>
2019-12-08 20:48:57 -0500

    why broken

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,79 +1,94 @@
-import React, {Component, Fragment} from 'react';
-import ReactDOM from 'react-dom';
-import ReactMapGL, { StaticMap } from 'react-map-gl';
-import axios from 'axios';
-import {Button} from 'reactstrap';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
 
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
-export default class Map extends Component {
+class Map extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.map = null;
-        this.state = {
-            init: 0,
-            viewport: {
-                latitude: 5,
-                longitude: 34,
-                zoom: 2,
-                bearing:0,
-                pitch: 0,
-                width: 500,
-                height: 500
-            }
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -122.48383155304096,
+      lat: 37.82882682974591,
+      zoom: 1.5,
+      coordinates: [
+        [-122.4833858013153, 37.829607404976734],
+        [-122.4830961227417, 37.82932776098012],
+        [-122.4830746650696, 37.82932776098012],
+        [-122.48218417167662, 37.82889558180985],
+        [-122.48218417167662, 37.82890193740421],
+        [-122.48221099376678, 37.82868372835086],
+        [-122.4822163581848, 37.82868372835086],
+        [-122.48205006122589, 37.82801003030873]
+      ],
+      test: 5,
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom, coordinates } = this.state;
+    console.log( coordinates)
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom : 16,
+    });
+
+    map.on('load', function () {
+      map.addLayer({
+        'id': 'lines',
+        'type': 'line',
+        'source': {
+          'type': 'geojson',
+          'data': {
+            'type': 'FeatureCollection',
+            'features': [{
+              'type': 'Feature',
+              'properties': {
+                'color': '#F7455D' // red
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': coordinates
+              }
+            },]
+          }
+        },
+        'paint': {
+          'line-width': 3,
+          // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
+          // to set the line-color to a feature property value.
+          // 'line-color': ['get', 'color']
         }
-    }
+      });
+    });
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
 
-    componentDidMount(){
-        const map = this.reactMap.getMap();
-        map.on('load', () => {
-         //add the GeoJSON layer here
-         map.addLayer({
-            "type": "Feature",
-            "geometry": {
-              "type": "LineString",
-              "coordinates": [[42.2781674, -71.3733123], [42.2781859, -71.3740851], [42.2781789, -71.3743694], [42.2781665, -71.3751255], [42.2781516, -71.3756383], [42.2781568, -71.3763113], [42.2782976, -71.3766419], [42.2786926, -71.3773061], [42.2790774, -71.3777637]]
-            }
-        })
-        })
-    }
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
 
-    setRoute(res){
-        this.setState({
-            init: 1,
-            viewport: {
-                ...this.viewport,
-                longitude: res.data.from_lat[1],
-                latitude: res.data.from_lat[0],
-                zoom: 14
-            }
-        })
-        this.renderLayers()
-    }
+  render() {
+    const { lng, lat, zoom } = this.state;
 
-    getRoute(){
-        axios
-        .get("http://ec2-3-85-127-123.compute-1.amazonaws.com:8000/simple/route/216,%20Pond%20Street,%20Natick,%20Massachusetts,%2001760,%20United%20States/14,%20Mill%20Street,%20Natick,%20Massachusetts,%2001760,%20United%20States")
-        .then(res => this.setRoute(res))
-        .catch(err => console.log(err));
-    }
-
-    render(){
-        const {viewport} = this.state;
-        return(
-            <React.Fragment>
-            <ReactMapGL
-                    ref={(reactMap) => this.reactMap = reactMap} />
-                    {{...viewport}}
-                    mapboxApiAccessToken='pk.eyJ1IjoiZHNhbmF0YXIiLCJhIjoiY2szcm81eTlpMGVndjNja2l5cGtobnRuMyJ9.0c8156xZy5h97w6Uq-f8wg'
-                    onViewportChange={newViewport => {
-                        this.setState({ viewport: newViewport });
-                    }}
-            />
-            <Button className="test" onClick={() => this.getRoute()}>Test</Button>
-        </React.Fragment>
-        )
-    }
-
+    return (
+      <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+      </div>
+    );
+  }
 }
 
+// ReactDOM.render(<Application />, document.getElementById('app'));
+export default Map;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,33 +24,9 @@ import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
 import LocationSearch from './components/LocationSearch';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZHNhbmF0YXIiLCJhIjoiY2szcm81eTlpMGVndjNja2l5cGtobnRuMyJ9.0c8156xZy5h97w6Uq-f8wg';
 
-class Application extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            lng: 5,
-            lat: 34,
-            zoom: 2
-        }
-    }
 
-    render(){
-        return(
-            <div>
-                <div ref={el => this.mapContainer = el} className="mapContainer" />
-                <Button onClick={() => this.getRoute()}>Test</Button>
-                <Map />
-                <LocationSearch />
-                <Sidebar/>
-            </div>
-
-        );
-    }
-}
-
-ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
