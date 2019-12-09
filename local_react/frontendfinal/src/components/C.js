f1387b7be232c5bfaa47f71a1fb5b61e5e7f2ad5
Eden Mendel <mendeleden@gmail.con>
2019-12-09 15:25:30 -0500

    heyo

--- local_react/frontendfinal/src/components/C.js
+++ local_react/frontendfinal/src/components/C.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
+import {
+    Button,
+} from "reactstrap";
+import axios from 'axios';
+
 class C extends React.Component {
     constructor(props) {
       super(props) 
       console.log(this.props.name) // nnamdi
+
+
     }
   
+    handleSelect = async value => {
+        this.setAddressTo(value);
+    };
+    
+    handleSelectF = async value => {
+        this.setAddressFrom(value);
+    };
+
+    gen_values() {
+        this.setRoute.then(res => { console.log(res)})
+        return [42.27822345, -71.37579087084606];
+      }
+
     render() {
         return (
             <div>
                 C component
                 <button onClick={
                   (evt) => 
-                    this.props.func([42.27822345, -71.37579087084606])
+                    this.props.func(this.gen_values())
                   }>
                   Send To Parent
                 </button>
