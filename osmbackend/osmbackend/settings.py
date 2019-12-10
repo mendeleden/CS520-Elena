6b3a9579766f6e4185631d36b3be8371cd9818b1
Eden Mendel <mendeleden@gmail.con>
2019-12-09 21:34:45 -0500

    addign new address

--- osmbackend/osmbackend/settings.py
+++ osmbackend/osmbackend/settings.py
@@ -126,4 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'ec2-3-85-127-123.compute-1.amazonaws.com', '3.85.127.123']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'ec2-3-85-127-123.compute-1.amazonaws.com', '3.85.127.123', 'ec2-52-90-226-146.compute-1.amazonaws.com']
