0806dbe42bd23e2a7d2cc94dde41da45185b1304
Eden Mendel <mendeleden@gmail.con>
2019-12-15 18:33:11 -0500

    push frontend for local and cloud deployments

--- local/Frontend/src/App.js
+++ local/Frontend/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import M from "./components/M"
+import LocationSearch from "./components/C"
+import G from "./components/G"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.output = this.output.bind(this)
+    this.state = {
+      data: 0,
+      lng: -122.48383155304096,
+      lat: 37.82882682974591,
+      steps : [],
+      elevation : [],
+    }
+  }
+  
+  async output(evt){
+    let test = await evt
+    evt.then(res=>{
+      this.setState({
+        lng: res.data.midpoint_lon,
+        lat:res.data.midpoint_lat, 
+        steps : res.data.lat_lon_steps,
+        elevation : res.data.elevation,
+      })
+    })
+  }
+  render() {
+      return (
+          <div>
+            <LocationSearch func={this.output}/>
+
+            <M 
+              mid_lat={this.state.lat} 
+              mid_lon={this.state.lng}
+              steps = {this.state.steps}
+              />
+            
+            <G elevation={this.state.elevation} />
+          </div>
+      )
+  }
 }
 
 export default App;
